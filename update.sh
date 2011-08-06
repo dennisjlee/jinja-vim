0668681b2d326f591cc24f69839be975ab50dfa3
Eivind Uggedal <eivind@uggedal.com>
2011-08-07 00:59:32 +0200

    Initial import.

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Updates the contents of this repo with changes from upstream.
+
+mkdir syntax && cd syntax && wget -O - https://github.com/mitsuhiko/jinja2/tarball/master | tar xz --strip-components 3 --wildcards --no-anchored "*/ext/Vim"
+
+if [ "$(git status --porcelain)" ]; then
+  git add . && git commit -am "Changes from upstream."
+fi
