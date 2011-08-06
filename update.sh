7e4f472df76afa9ff485568de6df26ad15d156cd
Eivind Uggedal <eivind@uggedal.com>
2011-08-07 01:01:39 +0200

    Changes from upstream.

--- update.sh
+++ update.sh
@@ -2,7 +2,11 @@
 #
 # Updates the contents of this repo with changes from upstream.
 
-mkdir syntax && cd syntax && wget -O - https://github.com/mitsuhiko/jinja2/tarball/master | tar xz --strip-components 3 --wildcards --no-anchored "*/ext/Vim"
+if [ -d syntax ]; then
+  mkdir syntax
+fi
+
+cd syntax && wget -O - https://github.com/mitsuhiko/jinja2/tarball/master | tar xz --strip-components 3 --wildcards --no-anchored "*/ext/Vim"
 
 if [ "$(git status --porcelain)" ]; then
   git add . && git commit -am "Changes from upstream."
